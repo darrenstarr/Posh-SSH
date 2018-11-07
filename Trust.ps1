c449f3b9813921f4116379c478c0a0dd687904ab
Darren R. Starr <darren@nocturnal.no>
2018-11-07 13:57:35 +0100

    Functional Linux port of Posh-SSH

--- Trust.ps1
+++ Trust.ps1
@@ -1,114 +1,116 @@
 ﻿# .ExternalHelp Posh-SSH.psm1-Help.xml
- function Get-SSHTrustedHost
- {
-     [CmdletBinding()]
-     [OutputType([int])]
-     Param()
+function Get-SSHTrustedHost
+{
+    [CmdletBinding()]
+    [OutputType([int])]
+    Param()
 
-     Begin{}
-     Process
-     {
-        $Test_Path_Result = Test-Path -Path "hkcu:\Software\PoshSSH"
-        if ($Test_Path_Result -eq $false)
-        {
-            Write-Verbose -Message 'No previous trusted keys have been configured on this system.'
-            New-Item -Path HKCU:\Software -Name PoshSSH | Out-Null
-            return
-        }
-        $poshsshkey = [Microsoft.Win32.Registry]::CurrentUser.OpenSubKey('Software\PoshSSH', $true)
-
-        $hostnames = $poshsshkey.GetValueNames()
-        $TrustedHosts = @()
-        foreach($h in $hostnames)
-        {
-            $TrustedHost = @{
-                SSHHost        = $h
-                Fingerprint = $poshsshkey.GetValue($h)
+    Begin{}
+    Process
+    {
+        if($IsWindows) {
+            $Test_Path_Result = Test-Path -Path "hkcu:\Software\PoshSSH"
+            if ($Test_Path_Result -eq $false) {
+                Write-Verbose -Message 'No previous trusted keys have been configured on this system.'
+                New-Item -Path HKCU:\Software -Name PoshSSH | Out-Null
+                return
             }
-            $TrustedHosts += New-Object -TypeName psobject -Property $TrustedHost
+            $poshsshkey = [Microsoft.Win32.Registry]::CurrentUser.OpenSubKey('Software\PoshSSH', $true)
+
+            $hostnames = $poshsshkey.GetValueNames()
+            $TrustedHosts = @()
+            foreach($h in $hostnames) {
+                $TrustedHost = @{
+                    SSHHost        = $h
+                    Fingerprint = $poshsshkey.GetValue($h)
+                }
+                $TrustedHosts += New-Object -TypeName psobject -Property $TrustedHost
+            }
+        } else {
+            throw "Platform not supported yet"
         }
-     }
-     End
-     {
+    }
+    End
+    {
         $TrustedHosts
-     }
+    }
  }
 
 
 # .ExternalHelp Posh-SSH.psm1-Help.xml
- function New-SSHTrustedHost
- {
+function New-SSHTrustedHost
+{
     [CmdletBinding()]
-     Param
-     (
-         # IP Address of FQDN of host to add to trusted list.
-         [Parameter(Mandatory=$true,
-                    ValueFromPipelineByPropertyName=$true,
-                    Position=0)]
-         $SSHHost,
+    Param
+    (
+        # IP Address of FQDN of host to add to trusted list.
+        [Parameter(Mandatory=$true,
+        ValueFromPipelineByPropertyName=$true,
+        Position=0)]
+        $SSHHost,
 
-         # SSH Server Fingerprint.
-         [Parameter(Mandatory=$true,
-                    ValueFromPipelineByPropertyName=$true,
-                    Position=1)]
-         $FingerPrint
-     )
+        # SSH Server Fingerprint.
+        [Parameter(Mandatory=$true,
+        ValueFromPipelineByPropertyName=$true,
+        Position=1)]
+        $FingerPrint
+    )
 
-     Begin
-     {
-     }
-     Process
-     {
-        $softkey = [Microsoft.Win32.Registry]::CurrentUser.OpenSubKey('Software', $true)
-        if ( $softkey.GetSubKeyNames() -contains 'PoshSSH')
-        {
-            $poshsshkey = [Microsoft.Win32.Registry]::CurrentUser.OpenSubKey('Software\PoshSSH', $true)
+    Begin
+    {
+    }
+    Process
+    {
+        if($IsWindows) {
+            $softkey = [Microsoft.Win32.Registry]::CurrentUser.OpenSubKey('Software', $true)
+            if ( $softkey.GetSubKeyNames() -contains 'PoshSSH') {
+                $poshsshkey = [Microsoft.Win32.Registry]::CurrentUser.OpenSubKey('Software\PoshSSH', $true)
+            } else {
+                Write-Verbose 'PoshSSH Registry key is not present for this user.'
+                New-Item -Path HKCU:\Software -Name PoshSSH | Out-Null
+                Write-Verbose 'PoshSSH Key created.'
+                $poshsshkey = [Microsoft.Win32.Registry]::CurrentUser.OpenSubKey('Software\PoshSSH', $true)
+            }
+
+            Write-Verbose "Adding to trusted SSH Host list $($SSHHost) with a fingerprint of $($FingerPrint)"
+            $poshsshkey.SetValue($SSHHost, $FingerPrint)
+            Write-Verbose 'SSH Host has been added.'
+        } else {
+            throw "Platform not supported yet"
         }
-        else
-        {
-            Write-Verbose 'PoshSSH Registry key is not present for this user.'
-            New-Item -Path HKCU:\Software -Name PoshSSH | Out-Null
-            Write-Verbose 'PoshSSH Key created.'
-            $poshsshkey = [Microsoft.Win32.Registry]::CurrentUser.OpenSubKey('Software\PoshSSH', $true)
-        }
-        Write-Verbose "Adding to trusted SSH Host list $($SSHHost) with a fingerprint of $($FingerPrint)"
-        $poshsshkey.SetValue($SSHHost, $FingerPrint)
-        Write-Verbose 'SSH Host has been added.'
-     }
-     End
-     {
-     }
- }
+    }
+    End
+    {
+    }
+}
 
 # .ExternalHelp Posh-SSH.psm1-Help.xml
- function Remove-SSHTrustedHost
- {
+function Remove-SSHTrustedHost
+{
     [CmdletBinding()]
-     Param
-     (
-         # Param1 help description
-         [Parameter(Mandatory=$true,
-                    ValueFromPipelineByPropertyName=$true,
-                    Position=0)]
-         [string]
-         $SSHHost
-     )
+    Param
+    (
+        # Param1 help description
+        [Parameter(Mandatory=$true,
+            ValueFromPipelineByPropertyName=$true,
+            Position=0)]
+        [string]
+        $SSHHost
+    )
 
-     Begin
-     {
-     }
-     Process
-     {
+    Begin
+    {
+    }
+    Process
+    {
         $softkey = [Microsoft.Win32.Registry]::CurrentUser.OpenSubKey('Software', $true)
-        if ($softkey.GetSubKeyNames() -contains 'PoshSSH' )
-        {
+        if ($softkey.GetSubKeyNames() -contains 'PoshSSH' ) {
             $poshsshkey = [Microsoft.Win32.Registry]::CurrentUser.OpenSubKey('Software\PoshSSH', $true)
-        }
-        else
-        {
+        } else {
             Write-warning 'PoshSSH Registry key is not present for this user.'
             return
         }
+
         Write-Verbose "Removing SSH Host $($SSHHost) from the list of trusted hosts."
         if ($poshsshkey.GetValueNames() -contains $SSHHost)
         {
@@ -119,6 +121,6 @@
         {
             Write-Warning "SSH Hosts $($SSHHost) was not present in the list of trusted hosts."
         }
-     }
-     End{}
- }
+    }
+    End{}
+}
--- TrustedKeyMng.cs
+++ TrustedKeyMng.cs
@@ -3,6 +3,8 @@ namespace SSH
     using System.Collections.Generic;
     using Microsoft.Win32;
     using System;
+    using System.IO;
+    using Newtonsoft.Json;
 
     // Class for managing the keys 
     public class TrustedKeyMng
@@ -11,44 +13,87 @@ namespace SSH
 
         public Dictionary<string, string> GetKeys()
         {
-            return Keys;
+            if(Environment.OSVersion.Platform == PlatformID.Unix)
+            {
+                var hostkeys = new Dictionary<string, string>(StringComparer.InvariantCultureIgnoreCase);
 
-            // var hostkeys = new Dictionary<string, string>(StringComparer.InvariantCultureIgnoreCase);
-            // var poshSoftKey = Registry.CurrentUser.OpenSubKey(@"Software\PoshSSH", true);
-            // if (poshSoftKey != null)
-            // {
-            //     var hosts = poshSoftKey.GetValueNames();
-            //     foreach (var host in hosts)
-            //     {
-            //         var hostkey = poshSoftKey.GetValue(host).ToString();
-            //         hostkeys.Add(host, hostkey);
-            //     }
-            // }
-            // else
-            // {
-            //     using (var softKey = Registry.CurrentUser.OpenSubKey(@"Software", true))
-            //     {
-            //         if (softKey != null) softKey.CreateSubKey("PoshSSH");
-            //     }
-            // }
-            // return hostkeys;
+                var personalFolderPath = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+                var poshSshFolderPath = Path.Combine(personalFolderPath, ".poshssh");
+                var poshSshKeysPath = Path.Combine(poshSshFolderPath, "known_hosts");
+
+                if(File.Exists(poshSshKeysPath))
+                {
+                    var keysText = File.ReadAllText(poshSshKeysPath);
+                    hostkeys = JsonConvert.DeserializeObject<Dictionary<string, string>>(keysText);
+                }
+
+                return hostkeys;
+            }
+            else if(Environment.OSVersion.Platform == PlatformID.Win32NT)
+            {
+                var hostkeys = new Dictionary<string, string>(StringComparer.InvariantCultureIgnoreCase);
+                var poshSoftKey = Registry.CurrentUser.OpenSubKey(@"Software\PoshSSH", true);
+                if (poshSoftKey != null)
+                {
+                    var hosts = poshSoftKey.GetValueNames();
+                    foreach (var host in hosts)
+                    {
+                        var hostkey = poshSoftKey.GetValue(host).ToString();
+                        hostkeys.Add(host, hostkey);
+                    }
+                }
+                else
+                {
+                    using (var softKey = Registry.CurrentUser.OpenSubKey(@"Software", true))
+                    {
+                        if (softKey != null) softKey.CreateSubKey("PoshSSH");
+                    }
+                }
+                return hostkeys;
+            }
+            else
+            {
+                throw new Exception("Platform not supported");
+            }
         }
 
         public bool SetKey(string host, string fingerprint)
         {
-            Keys[host] = fingerprint;
-            return  true;
-            // var poshSoftKey = Registry.CurrentUser.OpenSubKey(@"Software\PoshSSH", true);
-            // if (poshSoftKey != null)
-            // {
-            //     poshSoftKey.SetValue(host, fingerprint);
-            //     return true;
-            // }
-            // var softKey = Registry.CurrentUser.OpenSubKey(@"Software", true);
-            // if (softKey == null) return true;
-            // softKey.CreateSubKey("PoshSSH");
-            // softKey.SetValue(host, fingerprint);
-            // return true;
+            if (Environment.OSVersion.Platform == PlatformID.Unix)
+            {
+                var hostkeys = GetKeys();
+                hostkeys[host] = fingerprint;
+
+                var personalFolderPath = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+                var poshSshFolderPath = Path.Combine(personalFolderPath, ".poshssh");
+                if (!Directory.Exists(poshSshFolderPath))
+                    Directory.CreateDirectory(poshSshFolderPath);
+
+                var poshSshKeysPath = Path.Combine(poshSshFolderPath, "known_hosts");
+
+                var keysText = JsonConvert.SerializeObject(hostkeys);
+                File.WriteAllText(poshSshKeysPath, keysText);
+
+                return true;
+            }
+            else if (Environment.OSVersion.Platform == PlatformID.Win32NT)
+            {
+                var poshSoftKey = Registry.CurrentUser.OpenSubKey(@"Software\PoshSSH", true);
+                if (poshSoftKey != null)
+                {
+                    poshSoftKey.SetValue(host, fingerprint);
+                    return true;
+                }
+                var softKey = Registry.CurrentUser.OpenSubKey(@"Software", true);
+                if (softKey == null) return true;
+                softKey.CreateSubKey("PoshSSH");
+                softKey.SetValue(host, fingerprint);
+                return true;
+            }
+            else
+            {
+                throw new Exception("Platform not supported");
+            }
         }
     }
 }
